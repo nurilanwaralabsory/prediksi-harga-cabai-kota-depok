8c19c09700ad012cd014dc090b2bb8530ed71507
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2026-06-05 21:57:30 +0700

    feat: add backend

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = joblib.load("model_cabai_rf.pkl")
+
+class PrediksiInput(BaseModel):
+     Tahun: int
+     Bulan: int
+     Hari: int
+     Hari_dalam_tahun: int
+     Lag_1: float
+     Lag_7: float
+     Rolling_Mean_7: float
+
+@app.post("/predict")
+def predict_harga(data: PrediksiInput):
+     input_data = pd.DataFrame([data.dict()])
+     hasil = model.predict(input_data)
+     return {"prediksi_harga": hasil[0]}
+
+@app.get("/")
+def read_root():
+     return {"message": "API Prediksi Harga Cabai Aktif!"}
