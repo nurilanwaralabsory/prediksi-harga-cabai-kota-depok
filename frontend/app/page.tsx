f487b257b9f6988349936efb2f64b775c77dfc4f
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2026-06-08 13:19:44 +0700

    feat: add cursor pointer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
                </h1>
                <button
                     onClick={tanganiPrediksi}
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                    className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
                >
                     Cek Prediksi Besok
                </button>
